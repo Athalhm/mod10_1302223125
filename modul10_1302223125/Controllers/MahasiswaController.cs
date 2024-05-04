0f085af1a38103edb00a70087731e88cd7f70a13
Athallah az <athallah.az@gmail.com>
2024-05-04 08:35:58 +0700

    first commit

---
+++ modul10_1302223125/Controllers/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul10_1302223125.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        // GET: api/<MahasiswaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MahasiswaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MahasiswaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MahasiswaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MahasiswaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ modul10_1302223125/Mahasiswa.cs
@@ -0,0 +1,6 @@
+﻿namespace modul10_1302223125
+{
+    public class Mahasiswa
+    {
+    }
+}
